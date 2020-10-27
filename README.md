b3e4d5d904ee2bc29fd22ac9a2b9e711871e6cd8
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2020-10-27 16:57:54 +0800

    zanyuan commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# PlanB_frontend
+PlanB frontend
+1. Finish implementing the signUp and logIn page.
+2. Design the main student page and tutor page.
---
+++ app/src/main/java/com/example/planb/StudentPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.planb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_page);
+    }
+}
